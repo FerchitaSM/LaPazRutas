cbf52b9e1cfb550a7954d43a170ecec37f77eea5
Carlos Cavero <ccaverotx@icloud.com>
2019-10-05 13:56:05 -0400

    Se añadieron algunos comandos basicos.

--- src/main/java/LaPazRutasBot.java
+++ src/main/java/LaPazRutasBot.java
@@ -3,8 +3,32 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
 public class LaPazRutasBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage().getText()); // texto de prueba
-        System.out.println(update.getMessage().getFrom().getId()); // id para mantener la conversacion
+//        System.out.println(update.getMessage().getText()); // texto de prueba
+//        System.out.println(update.getMessage().getFrom().getId()); // id para mantener la conversacion
+
+        String command=update.getMessage().getText();
+
+        SendMessage message = new SendMessage();
+
+        if(command.equals("/myname")){
+
+            System.out.println(update.getMessage().getFrom().getFirstName());
+            message.setText(update.getMessage().getFrom().getFirstName());
+        }
+
+        if (command.equals("/mylastname")){
+
+            System.out.println(update.getMessage().getFrom().getLastName());
+            message.setText(update.getMessage().getFrom().getLastName());
+        }
+
+        if (command.equals("/myfullname")){
+            System.out.println(update.getMessage().getFrom().getFirstName()+" "+update.getMessage().getFrom().getLastName());
+            message.setText(update.getMessage().getFrom().getFirstName()+" "+update.getMessage().getFrom().getLastName());
+        }
+
+        message.setChatId(update.getMessage().getChatId());
+
     }
 
     public String getBotUsername() {
