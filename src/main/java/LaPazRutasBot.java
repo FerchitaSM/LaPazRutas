91bddda6032c5860ad17330af2b8c4ead3c1b4d3
FerchitaSM <sarmiento.fernada.jehovana@gmail.com>
2019-10-05 13:29:38 -0400

    Enviando el mismo mensaje de vuelta

--- src/main/java/LaPazRutasBot.java
+++ src/main/java/LaPazRutasBot.java
@@ -1,10 +1,19 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class LaPazRutasBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage().getText()); // texto de prueba
-        System.out.println(update.getMessage().getFrom().getId()); // id para mantener la conversacion
+        final String TextReceived = update.getMessage().getText(); // texto del usuario
+        final long chatId = update.getMessage().getChatId(); // id del usuario
+
+        SendMessage message = new SendMessage().setChatId(chatId).setText(TextReceived); // Crear mensaje para enviar
+        try {
+            execute(message); //Envio del mensaje
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getBotUsername() {
